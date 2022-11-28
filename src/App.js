313ed18d2a044c09d989637038d413599f21678c
Cameron Miner <cameronsminer@gmail.com>
2022-11-28 16:53:44 -0700

    Added declared winner functionality

--- src/App.js
+++ src/App.js
@@ -11,6 +11,27 @@ const App = () => {
     setPlayer(true)
   }
 
+  const calculateWinner = (arr) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+        return `${arr[a]} won!`
+      }
+    }
+    return "Who will win?"
+  }
+
   return (
     <div className="App">
       <div className="container">
@@ -28,6 +49,7 @@ const App = () => {
         })}
       </div>
       <button onClick={handleClick}>Reset</button>
+      <span>{calculateWinner(squares)}</span>
     </div>
   );
 };
